180259c97720a1cb2a5213f8bb2a690dbb53a7c4
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-16 22:42:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,6 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:known_for] = "GUI"
+		 programmer_hash
 
 end
 
@@ -126,4 +127,5 @@ def adding_to_dennis
         }
      }
 	programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+	programmer_hash
 end
